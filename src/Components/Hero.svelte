b56dd48592dfd3f82f982cce3b256fc14b3009ba
Adam Mellen <github@mellen.io>
2021-09-15 19:08:14 +0100

    Updating hero to better match design image

--- src/Components/Hero.svelte
+++ src/Components/Hero.svelte
@@ -3,16 +3,26 @@
 </script>
 
 <section class="hero is-fullheight">
+	<div class="cover">
+		<svg version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+			 viewBox="0 0 100 100" width="100%" height="100%" preserveAspectRatio="none" xml:space="preserve">
+<style type="text/css">
+	.st0{fill:#56246A;}
+    .st1{fill:#73C9B8;}
+</style>
+			<polygon id="XMLID_1_" class="st0" points="50,100 100,80 100,100 "/>
+			<polygon id="XMLID_3_" class="st1" points="50,0 0,20 0,0 "/>
+</svg>
+
+	</div>
+	<div class="big-dino">
+		<img src="./images/big-dino.png"/>
+	</div>
 	<div class="hero-body">
 		<div class="container">
 			<div class="columns is-vcentered is-reverse-mobile">
 				<div class="column is-half-desktop">
 					<h1 class="title">Dinosols NFT</h1>
-				</div>
-				<div class="column is-half-desktop">
-					<figure class="image is-square logo-image">
-						<img src="./images/logo.png" />
-					</figure>
 				</div>
 			</div>
 		</div>
@@ -28,9 +38,25 @@
   @import '../theme/_variables.scss';
   .hero {
 	background-color: $purple;
-  }
+	position: relative;
+	.big-dino {
+	  position: absolute;
+	  bottom: 0;
+	  right: 0;
+	  //width: 50vw;
+	  z-index: 1;
+	}
+    .cover {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      z-index: 2;
+    }
 
-  .title {
-	font-size: 5rem;
+	.hero-body {
+	  z-index: 3;
+	}
   }
 </style>
